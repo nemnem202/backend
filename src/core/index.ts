df18c5d098714a9aae2e6570257ada64077bb61d
nemnem202 <naimelhabbas@gmail.com>
2025-10-06 14:05:10 +0200

    init

---
+++ src/core/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello depuis TSX 👋");
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
