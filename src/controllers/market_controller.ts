7567429e5f03f41bc300958892f4368b22bce6b5
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-10 15:32:29 +0200

    Modify postedProduct types to ProductDTO types Send postedProduct to productRepository Display error if so during parsing

--- src/controllers/market_controller.ts
+++ src/controllers/market_controller.ts
@@ -1,35 +1,48 @@
 import { Request, Response } from "express";
 import { ServerResponse } from "../types/general/server_response";
-import { Product } from "../types/tables/product";
+import { Product, ProductDTO } from "../types/tables/product";
 import { ZodSchema } from "../lib/zod_schemas";
+import { ProductRepository } from "../repository/product";
+import { ZodError } from "zod";
+
 
 export class MarketController {
+  static productRepository = new ProductRepository();
+
   static find(req: Request, res: Response<ServerResponse>) {
     const response: ServerResponse = {
       message: "market !",
       success: true,
     };
 
+
     res.send(response);
   }
 
-  createProduct(req:Request, res:Response<ServerResponse>){
+  static async createProduct(req: Request, res: Response<ServerResponse>) {
 
-    const postedProduct:Product = {
-      account_id: req.body.account_id,
-      available_quantity: req.body.available_quantity,
-      id: req.body.id,
-      number_of_reports: 0,
-      number_of_sells: 0,
-      product_description: req.body.product_description,
-      product_image_path: req.body.product_image_path,
-      product_name: req.body.product_name,
-      product_price: req.body.product_price,
-      suspended: false,
+    try {
+
+      const postedProduct: ProductDTO = {
+        account_id: req.body.account_id,
+        available_quantity: req.body.available_quantity,
+        number_of_reports: 0,
+        number_of_sells: 0,
+        product_description: req.body.product_description,
+        product_image_path: req.body.product_image_path,
+        product_name: req.body.product_name,
+        product_price: req.body.product_price,
+        suspended: false,
+      }
+
+      const validation = ZodSchema.product.safeParse(postedProduct)
+      const product = await this.productRepository.add_item(postedProduct)
+    } 
+    catch(result){
+      if(result instanceof ZodError){
+        console.log(result.issues);
+      }
     }
-
-    const validation = ZodSchema.product.safeParse(postedProduct)
-
   }
 
 }
