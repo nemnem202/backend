7b18998699ceaf1eb26ebda3d2984129ddd683d6
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-10 15:01:15 +0200

    Create object postedProduct who received params from request after posting forms Pass this object into ZodSchema to parse data

--- src/controllers/market_controller.ts
+++ src/controllers/market_controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { ServerResponse } from "../types/general/server_response";
+import { Product } from "../types/tables/product";
+import { ZodSchema } from "../lib/zod_schemas";
 
 export class MarketController {
   static find(req: Request, res: Response<ServerResponse>) {
@@ -10,4 +12,24 @@ export class MarketController {
 
     res.send(response);
   }
+
+  createProduct(req:Request, res:Response<ServerResponse>){
+
+    const postedProduct:Product = {
+      account_id: req.body.account_id,
+      available_quantity: req.body.available_quantity,
+      id: req.body.id,
+      number_of_reports: 0,
+      number_of_sells: 0,
+      product_description: req.body.product_description,
+      product_image_path: req.body.product_image_path,
+      product_name: req.body.product_name,
+      product_price: req.body.product_price,
+      suspended: false,
+    }
+
+    const validation = ZodSchema.product.safeParse(postedProduct)
+
+  }
+
 }
