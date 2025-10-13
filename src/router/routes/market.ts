4b56895367f6205c089a3b3cf123db88a8344a87
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-13 21:39:36 +0200

    Fixing minor change to merge without any conflicts (I hope)

--- src/router/routes/market.ts
+++ src/router/routes/market.ts
@@ -6,12 +6,7 @@ const market = Router();
 
 import AuthMiddleWare from "../../lib/middlewares/auth";
 
-const market = Router();
-
-market.get(
-  "",
-  (req, res, next) => AuthMiddleWare.protect_user_route(req, res, next),
-market.post("/create-product", uploadMiddleWare.single('productImage') ,(req, res) => MarketController.createProduct(req, res));
-);
+market.get("", (req, res, next) => AuthMiddleWare.protect_user_route(req, res, next));
+market.post("/create-product", uploadMiddleWare.single('productImage') ,(req, res) => MarketController.createProduct(req, res))
 
 export default market;
