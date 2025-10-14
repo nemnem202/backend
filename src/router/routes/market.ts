f4ddf5119bad42fc7bb5c65101ef6945b6499256
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-14 16:47:43 +0200

    Allow user to send an image with his product Upload image on the cloud and store link into the database

--- src/router/routes/market.ts
+++ src/router/routes/market.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { MarketController } from "../../controllers/market_controller";
 import AuthMiddleWare from "../../lib/middlewares/auth";
+import uploadImage from "../../lib/middlewares/config-multer";
 
 const market = Router();
 
@@ -9,5 +10,20 @@ market.get(
   (req, res, next) => AuthMiddleWare.protect_user_route(req, res, next),
   (req, res) => MarketController.find(req, res)
 );
+
+/** Think to reanable this route and delete the next one */
+
+/** market.post(
+  "/create-product",
+  (req, res, next) => AuthMiddleWare.protect_vendor_route(req, res, next),
+  (req, res) => MarketController.createProduct(req, res)
+); **/
+
+market.post(
+  "/create-product",
+  uploadImage.single('productImage'),
+  (req, res) => MarketController.createProduct(req, res)
+);
+
 
 export default market;
